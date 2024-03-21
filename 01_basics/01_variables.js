e2e1113a25d24c9a62fdb0df429840d36e7b2514
Mubashir Khan <162383206+MubashirKhan0@users.noreply.github.com>
2024-03-21 23:17:54 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "mubashir@google.com"
+var accountPassword ="12345"
+accountcity = "jaipur"
+
+// /accountID = 2 //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountcity = "karachi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountcity])
